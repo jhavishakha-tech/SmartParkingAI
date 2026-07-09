608a38a4366bfcad3500771a0b71b23542c24ade
Vishakha Jha <jhavishakha44@gmail.com>
2026-07-10 03:42:29 +0530

    Added Streamlit app

---
+++ streamlit_app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.set_page_config(page_title="Smart Parking AI")
+
+st.title("🚗 Smart Parking AI")
+
+st.write("Developer: Vishakha Jha")
+
+st.write("""
+This project uses YOLOv8 and OpenCV to detect occupied and free parking slots.
+
+### Features
+- Vehicle Detection using YOLOv8
+- Parking Slot Detection
+- Free & Occupied Slot Count
+- OpenCV Visualization
+
+Repository:
+https://github.com/jhavishakha-tech/SmartParkingAI
+""")
+
+st.success("Project Uploaded Successfully ✅")
